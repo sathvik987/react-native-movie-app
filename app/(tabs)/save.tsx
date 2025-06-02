17a7bd1dedbfe7c13c7e0abfcf0700aadbc95c52
Sathvik Patil <sathvikpatil67@gmail.com>
2025-06-02 19:54:36 +0530

    Added home and search view

--- app/(tabs)/save.tsx
+++ app/(tabs)/save.tsx
@@ -1,10 +1,22 @@
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 const Saved = () => {
     return (
-        <View>
-            <Text>Saved</Text>
+        <View className="flex-1 bg-primary">
+            <Image
+                source={images.bg}
+                className="flex-1 absolute w-full z-0"
+                resizeMode="cover"
+            />
+            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+            <View className='flex-1 justify-center items-center mt-2'>
+                <Text className="text-white text-2xl">Not impleted yet!</Text>
+            </View>
+
         </View>
     )
 }
