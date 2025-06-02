87ab9fc6ea076987879d7c0c773c00d3683b3969
Sathvik Patil <sathvikpatil67@gmail.com>
2025-06-02 21:32:59 +0530

    Added movie details

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,16 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
+
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-  </Stack>;
+  return (
+    <SafeAreaProvider>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
